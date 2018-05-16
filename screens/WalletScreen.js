5b02d060b1578f7a0ef4656b949f3fae86c2d5ec
Brandon Holland <brandon.holland@catalystrms.com>
2018-05-15 23:47:58 -0700

    Added blockie to tokens, more wallet UI changes.

--- screens/WalletScreen.js
+++ screens/WalletScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StatusBar, SafeAreaView, View, Text, ScrollView, ActivityIndicator, RefreshControl, Animated } from 'react-native';
-import { List, ListItem } from 'react-native-elements';
-import { FontAwesome, Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import { StatusBar, SafeAreaView, View, Text, ScrollView, Animated, FlatList } from 'react-native';
+import { ListItem, Button } from 'react-native-elements';
+import { FontAwesome, Entypo, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { LinearGradient, AppLoading } from 'expo';
 import { HttpClient } from '@tronprotocol/wallet-api';
 
 import { TronLogoPathGraphic, TronLogoLineGraphic } from '../graphics/TronLogoGraphic.js';
+import Blockie from '../support/Blockie.js';
 
 const headerRight = (
   <View style={{ flex: 1, flexDirection: 'row' }}>
@@ -36,6 +37,7 @@ export default class WalletScreen extends React.Component {
       balance: '0.000',
       value: '0.00',
       tokens: [],
+      frozen: [],
       loaded: false
     }
 
@@ -64,6 +66,7 @@ export default class WalletScreen extends React.Component {
       balance: trxBalance,
       value: trxValue,
       tokens: tokens,
+      frozen: accountBalances.frozen.balances,
       loaded: true });
   }
 
@@ -74,6 +77,25 @@ export default class WalletScreen extends React.Component {
 
   componentDidMount() {
     this.reloadData();
+  }
+
+  renderTokenListItem = ({ item, i }) => {
+    return (
+      <ListItem
+        key={ i }
+        title={ item.name }
+        titleStyle={{ color: '#000000', fontSize: 16 }}
+        leftElement={<Blockie size={16} scale={2.0} seed={ item.name }/>}
+        rightTitle={ item.balance.toString() }
+        hideChevron
+        containerStyle={{
+          borderRadius: 8,
+          margin: 10,
+          marginBottom: 0,
+          borderBottomWidth: 0,
+          backgroundColor: '#ffffff'
+        }}/>
+    );
   }
 
   render() {
@@ -104,31 +126,28 @@ export default class WalletScreen extends React.Component {
           scrollIndicatorInsets={{ top: HEADER_MAX_HEIGHT }}
           scrollEventThrottle={ 16 }
           onScroll={ Animated.event([{ nativeEvent: { contentOffset: { y: this.scrollYAnimatedValue }}}], { userNativeDriver: true }) }>
-          <List containerStyle={{
-            marginTop: 0,
-            marginBottom: 10,
-            backgroundColor: null,
-            borderColor: null,
-            borderTopWidth: 0
-          }}>
-          { this.state.tokens.map((token, i) => (
-              <ListItem
-                key={ i }
-                title={ token.name }
-                titleStyle={{ color: '#000000', fontSize: 16 }}
-                leftIcon={{ name: 'coins', color: '#ca2b1e', type: 'material-community' }}
-                badge={{ value: token.balance, containerStyle: { backgroundColor: '#777777' } }}
-                hideChevron
-                containerStyle={{
-                  borderRadius: 8,
-                  margin: 10,
-                  marginBottom: 0,
-                  borderBottomWidth: 0,
-                  backgroundColor: '#ffffff'
-                }}/>
-            ))
-          }
-          </List>
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginLeft: 10, marginTop: 10, marginRight: 10 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <MaterialCommunityIcons name='coins' size={28} color='#333333'/>
+              <Text style={{ fontSize: 18, color: '#000000', marginLeft: 5 }}>Tokens</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <MaterialCommunityIcons
+              style={{ marginRight: 10 }}
+              name='swap-horizontal'
+              size={28}
+              color='#0f7acc'/>
+              <MaterialCommunityIcons
+                name='plus-circle-multiple-outline'
+                size={28}
+                color='#1aaa55'/>
+            </View>
+          </View>
+          <FlatList
+            style={{ marginBottom: 10 }}
+            keyExtractor={(item, index) => item + index}
+            renderItem={ this.renderTokenListItem }
+            data={ this.state.tokens }/>
         </ScrollView>
         <Animated.View
           style={{
@@ -171,6 +190,7 @@ export default class WalletScreen extends React.Component {
                 flex: 1,
                 flexDirection: 'row',
                 alignItems: 'center',
+                justifyContent: 'center',
                 paddingLeft: 10,
                 paddingRight: 10
               }}>
@@ -187,16 +207,6 @@ export default class WalletScreen extends React.Component {
                 <Text style={{ color: '#ffffff', opacity: 0.75, fontSize: 12 }}>(${ this.state.value })</Text>
               </View>
             </Animated.View>
-            <MaterialCommunityIcons
-              style={{
-                marginRight: 10,
-                position: 'absolute',
-                bottom: 10,
-                right: 0
-              }}
-              name='plus-circle-multiple-outline'
-              color='#ffffff'
-              size={28}/>
           </LinearGradient>
         </Animated.View>
       </SafeAreaView>
